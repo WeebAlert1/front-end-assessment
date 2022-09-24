5c885e6481dbcee0f8c61dcc8f032cf44ac86493
Keith Holdaway <keithholdaway1@hotmail.com>
2022-09-24 12:19:31 -0700

    Part 2-Edit JS fin

--- about.js
+++ about.js
@@ -5,8 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("From has been summited");
 }
+
+let cat = document.querySelector('img');
+
+function catAlert(evt) {
+	evt.preventDefault();
+
+	alert("Good job petting the Cat!");
+}
+
+cat.addEventListener('mouseover', catAlert);
+
 
 
 let form = document.querySelector('#contact');
