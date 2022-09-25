4458b9ca55fab3641243ba56bcbc8251ccd2faca
Keith Holdaway <keithholdaway1@hotmail.com>
2022-09-25 13:17:34 -0700

    started extra content

---
+++ profile.js
@@ -0,0 +1,15 @@
+let favColor = document.getElementById('color');
+let favPlace = document.getElementById('place');
+let favRitual = document.getElementById('ritual');
+
+favColor.addEventListener('click', () => {
+    alert('Forest Green');
+});
+
+favPlace.addEventListener('click', () => {
+    alert('Home');
+});
+
+favRitual.addEventListener('click', () => {
+    alert('Coming home and turning on the comupter');
+});
